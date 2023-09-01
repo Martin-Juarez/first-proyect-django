c9708ec99d012fc05bec2eb43bf19e345ccac9f8
Martin <tgomartinjuarez@gmail.com>
2023-08-31 20:37:31 -0600

    clase 4

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import get_koder
+from .views import get_koder,list_mentors
 
 
 urlpatterns = [
     path("koders/",get_koder),
+    path("mentors/",list_mentors),
     #path("koder/<int:key>",get_koders),
     
 ]
--- bootcamp/views.py
+++ bootcamp/views.py
@@ -7,12 +7,35 @@ from django.template import loader
 def get_koder(request):
     context = {
     "koders": [
-        {'name ': 'Martin','ID': '1'},
-        {'name ': 'Miguel','ID': '2'},
-        {'name ': 'Pedro','ID': '3'},
-        {'name ': 'Juan','ID': '4'},
-        {'name ': 'Pepe','ID': '5'}
+        {'name': 'Martin','ID': '1'},
+        {'name': 'Miguel','ID': '2'},
+        {'name': 'Pedro','ID': '3'},
+        {'name': 'Juan','ID': '4'},
+        {'name': 'Pepe','ID': '5'},
     ]}
     template = loader.get_template("templates/koders.html")
+    
+    return HttpResponse(template.render(context,request))
+
+def list_mentors(request):
+    context = {
+    'mentors': [
+            {
+                'name': 'Benjamin',
+                'last_name': 'Aguilar',
+                'is_active': True
+            },
+            {
+                'name': 'Alfredo',
+                'last_name': 'Altamirano',
+                'is_active': True
+            },
+            {
+                'name': 'Charles',
+                'last_name': 'Lopez',
+                'is_active': False
+            },
+        ]}
+    template = loader.get_template("templates/list_mentors.html")
     
     return HttpResponse(template.render(context,request))
